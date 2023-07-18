6dfcbd6340ede33f7e26c5d414d8c2f18372f73b
TAEJU LEE <92421686+gkekf123@users.noreply.github.com>
2023-07-18 11:11:19 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import NewsHome from "./component2/NewsHome";
+import Header from "./layout/Header";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Header />}>
+        <Route path="/" element={<NewsHome />}></Route>
+        <Route path="/:category" element={<NewsHome />}></Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
